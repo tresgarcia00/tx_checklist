2058ffca2e9ef2bf4e9405516415105eb6735dfc
Tres Garcia <tresgarcia00@gmail.com>
2020-10-06 11:49:45 -0400

    Add files via upload

---
+++ list.js
@@ -0,0 +1,8 @@
+// alert("working");
+
+// document.addEventListener('DOMContentLoaded', funtion (){
+//     document.querySelector('button').addEventListener('click, onclick, false')
+//     function onclick (){
+        
+//     }
+// })
